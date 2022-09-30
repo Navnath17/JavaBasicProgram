d401a53b90f41c9294c0363c1ed13a6245ca4f3e
Navnath Kalhatkar <navnath.kalhatkar@BBD.INTL>
2022-09-30 17:29:22 +0530

    string program

---
+++ src/main/java/com/startup/stringprogram/CheckIfStringContainsDigitsOnly.java
@@ -0,0 +1,25 @@
+package com.startup.stringprogram;
+
+public class CheckIfStringContainsDigitsOnly {
+
+	public static void main(String[] args) {
+		digitsOnlyString("111");
+		digitsOnlyString("111a 1");
+		digitsOnlyString("111 222");
+		digitsOnlyString("111L");
+
+	}
+
+	private static void digitsOnlyString(String string) {
+		if(string.matches("\\d+")) System.out.println("Digit Only String ::"+string);
+		
+		try {
+			long l = Long.parseLong(string);
+			System.out.println("Digit Only String ::"+string);
+		}catch(Exception e){
+			System.out.println("Non Digit Only String ::"+string);
+		}
+		
+	}
+
+}
